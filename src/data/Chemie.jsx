2fbb6f66bf4f75c269518bbf2e45e77e27b0f9b3
RobertRadla <robertradla93@gmail.com>
2025-08-07 22:14:27 +0200

    Chemistry lesson 1 update 1

--- src/data/Chemie.jsx
+++ src/data/Chemie.jsx
@@ -7,7 +7,7 @@ const chemie = {
   lessons: [
     
     {
-      title: 'Lekce 1 - Atom',
+      title: 'Lekce 1 - Atom (Část 1)',
       pdf: '/docs/chemie_doktor/Lekce1_Atom.pdf',
       image: 'Atom',
       
@@ -24,6 +24,82 @@ const chemie = {
           question: 'Z čeho se skládají atomy?',
           answer: 'Z jádra a elektronového obalu.',
         },
+        {
+          question: 'Z čeho se skládá jádro atomu?',
+          answer: 'Z protonů a neutronů.',
+        },
+        {
+          question: 'Jaký náboj mají protony, neutrony a elektrony?',
+          answer: 'Protony mají kladný náboj, neutrony jsou neutrální a elektrony mají záporný náboj.',
+        },
+        {
+          question: 'Jak se souhrně nazývají protony a neutrony?',
+          answer: 'Nukleony.',
+        },
+        {
+          question: 'Existují atomy, které nemají žádné neutrony?',
+          answer: 'Ano, například atom vodíku.',
+        },
+        {
+          question: 'Co tvoří hmotnost atomu?',
+          answer: 'Hmotnost jádra tvoří téměř celou hmotnost atomu.',
+        },
+        {
+          question: 'Jakou hmotnost má elektron?',
+          answer: 'Elektron má velmi malou hmotnost, přibližně 9.107 × 10^-31 kg (je cca 1800x lehčí než proton a neutron).',
+        },
+        {
+         question: 'Jakou hmotnost mají protony a neutrony?',
+          answer: 'Protony a neutrony mají přibližně stejnou hmotnost, která je asi 1,67 × 10^-27 kg.',
+        },
+        {
+         question: 'Co určuje identitu atomu?',
+          answer: 'Počet protonů v jádře atomu určuje jeho identitu a chemické vlastnosti.',
+        },
+        {
+         question: 'Čím je tvořen elektronový obal? ',
+          answer: 'Elektronový obal je tvořen elektrony, které obíhají kolem jádra atomu.',
+        },
+        {
+         question: 'O kolik větší je elektronový obal než jádro atomu?',
+          answer: 'Elektronový obal je zhruba 100 000x větší než jádro atomu.',
+        },
+        {
+         question: 'Co určuje identitu atomu?',
+          answer: 'Počet protonů v jádře atomu určuje jeho identitu a chemické vlastnosti.',
+        },
+        {
+         question: 'Jaké všechny síly působí mezi částicemi v atomu?',
+          answer: 'V atomu působí elektromagnetická síla, silná jaderná síla a slabá jaderná síla (teoreticky i gravitační, ale v zanedbatelném měřítku).',
+        },
+        {
+         question: 'Jaká hlavní síla působí mezi protony a neutrony?',
+          answer: 'Mezi protony a neutrony působí tzv. silná jaderná síla.',
+        },
+        {
+         question: 'Co je tzv. silná jaderná síla?',
+          answer: 'Je to neviditelná sílá, které působí na protony a neutrony v jádře atomu a drží je pohromadě.',
+        },
+        {
+         question: 'Proč nemohou protony a neutrony uniknout z jádra?',
+          answer: 'Protože na vzdálenost asi 1 femtometr (10⁻¹⁵ m) je silná jaderná síla velmi silná a přitahuje je k sobě.',
+        },
+        {
+         question: 'Co se stane, když jsou protony a neutrony v jádře příliš blízko?',
+          answer: 'Začnou se odpuzovat, protože silná jaderná síla má při extrémně malé vzdálenosti odpudivou složku.',
+        },
+        {
+         question: 'Působí na sebe elektrony a neutrony elektromagnetickou silou?',
+          answer: 'Ne, elektrony a neutrony nepůsobí na sebe elektromagnetickou silou, protože neutrony jsou neutrální a nemají žádný elektrický náboj.',
+        },
+        {
+         question: 'Jaké síly působí mezi nabitými částicemi (protony a elektrony)?',
+          answer: 'Mezi nabitými částicemi působí elektromagnetická síla (Coloumbova síla).',
+        },
+        {
+         question: 'Kdy elektroomagnetická síla částice odpuzuje a kdy přitahuje?',
+          answer: 'Elektromagnetická síla odpuzuje částice se stejným nábojem a přitahuje částice s opačným nábojem.',
+        },
       ],
     },
 
@@ -42,7 +118,7 @@ const chemie = {
 
 
     {
-      title: 'Lekce 2 - Periodická tabulka',
+      title: 'Lekce 1 - Atom (Část 2)',
       pdf: '/docs/chemie_doktor/Lekce1_Atom.pdf',
       image: 'FlaskConical',
       
@@ -59,7 +135,7 @@ const chemie = {
     },
 
     {
-      title: 'Lekce 3 - Chemická vazba',
+      title: 'Lekce 1 - Atom (Část 3)',
       pdf: '/docs/chemie_doktor/Lekce1_Atom.pdf',
       image: 'FlaskConical',
       
