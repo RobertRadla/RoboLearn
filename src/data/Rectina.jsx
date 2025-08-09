33f8ba46e494b0175c11caed29b1c68c83669072
RobertRadla <robertradla93@gmail.com>
2025-08-09 19:50:04 +0200

    Greek updated - lesson 1,2

--- src/data/Rectina.jsx
+++ src/data/Rectina.jsx
@@ -3,7 +3,7 @@
 const rectina = {
   
   name: 'Řečtina',
-  
+  ttsLang: 'el-GR',
   lessons: [
     
     {
@@ -12,10 +12,104 @@ const rectina = {
       image: '',
       
       questions: [
+        { question: 'Α α', answer: 'Alfa – čteme: "a"' },
+        { question: 'Β β', answer: 'Víta (dříve Beta) – čteme: "v"' },
+        { question: 'Γ γ', answer: 'Gama – čteme: "g" před a, o, u; "j" před e, i' },
+        { question: 'Δ δ', answer: 'Delta – čteme: "d"' },
+        { question: 'Ε ε', answer: 'Epsilon – čteme: krátké "e"' },
+        { question: 'Ζ ζ', answer: 'Zíta (dříve Zéta) – čteme: "z"' },
+        { question: 'Η η', answer: 'Íta – čteme: dlouhé "í"' },
+        { question: 'Θ θ', answer: 'Thíta – čteme: neznělé "th" jako v ang. "thin"' },
+        { question: 'Ι ι', answer: 'Iota – čteme: krátké nebo dlouhé "i"' },
+        { question: 'Κ κ', answer: 'Kappa – čteme: "k"' },
+        { question: 'Λ λ', answer: 'Lamda (dříve Lambda) – čteme: "l"' },
+        { question: 'Μ μ', answer: 'Mí – čteme: "m"' },
+        { question: 'Ν ν', answer: 'Ní – čteme: "n"' },
+        { question: 'Ξ ξ', answer: 'Ksí – čteme: "ks"' },
+        { question: 'Ο ο', answer: 'Omikron – čteme: krátké "o"' },
+        { question: 'Π π', answer: 'Pí – čteme: "p"' },
+        { question: 'Ρ ρ', answer: 'Ro – čteme: "r"' },
+        { question: 'Σ σ / ς', answer: 'Sigma – čteme: "s" (ς se píše na konci slova)' },
+        { question: 'Τ τ', answer: 'Taf – čteme: "t"' },
+        { question: 'Υ υ', answer: 'Ýpsilon – čteme: "i"' },
+        { question: 'Φ φ', answer: 'Fí – čteme: "f"' },
+        { question: 'Χ χ', answer: 'Chí – čteme: "ch" jako v něm. "Bach"' },
+        { question: 'Ψ ψ', answer: 'Psí – čteme: "ps"' },
+        { question: 'Ω ω', answer: 'Omega – čteme: dlouhé "ó"' }
+      ],
+    },
+    
+    {
+      title: 'Lekce 2',
+      pdf: '',
+      image: '',
+      
+      questions: [
         {
-          question: '',
-          answer: '',
+         question: 'Já',
+         answer: 'Εγώ',
         },
+        {
+         question: 'Ty',
+         answer: 'Εσύ',
+        },
+        {
+         question: 'On',
+         answer: 'Αυτός',
+        },
+        {
+         question: 'Ona',
+         answer: 'Αυτή',
+        },
+        {
+         question: 'Ono',
+         answer: 'Αυτό',
+        },
+        {
+         question: 'My',
+         answer: 'Εμείς',
+        },
+        {
+         question: 'Vy',
+         answer: 'Εσείς',
+        },
+        {
+         question: 'Oni',
+         answer: 'Αυτοί',
+        },
+        {
+         question: 'Ony',
+         answer: 'Αυτές',
+        },
+        {
+         question: 'Ona (střední rod)',
+         answer: 'Αυτά',
+        },
+        {
+         question: 'Jsem (já).',
+         answer: 'Είμαι.',
+        },
+        {
+         question: 'Jsi (ty).',
+         answer: 'Είσαι.',
+        },
+        {
+         question: 'Je (on, ona, ono).',
+         answer: 'Είναι.',
+        },
+        {
+         question: 'Jsme (my).',
+         answer: 'Είμαστε.',
+        },
+        {
+         question: 'Jste (vy).',
+         answer: 'Είστε.',
+        },
+        {
+         question: 'Jsou (oni, ony, ona).',
+         answer: 'Είναι.',
+        },
+        
       ],
     },
 
